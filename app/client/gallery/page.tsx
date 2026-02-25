b13461393775af258f17e08f6878874146926841
Kevin Lam <kevinlam@Alvins-MacBook-Air-2.local>
2026-02-24 21:36:44 -0800

    Create client gallery

--- app/client/gallery/page.tsx
+++ app/client/gallery/page.tsx
@@ -1,5 +1,12 @@
+import { redirect } from "next/navigation";
+import { getCurrentUser } from "@/lib/auth";
+import { getClientGallery } from "./actions";
 import { ClientGalleryPage } from "./GalleryPage";
 
-export default function Page() {
-  return <ClientGalleryPage />;
+export default async function Page() {
+  const user = await getCurrentUser();
+  if (!user) redirect("/login");
+
+  const data = await getClientGallery();
+  return <ClientGalleryPage data={data} />;
 }

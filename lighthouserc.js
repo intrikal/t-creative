b149ddb0abccc590d817389743ee9b41ddb374bc
Kevin Lam <retrobytetech@gmail.com>
2026-03-25 18:30:25 -0700

    Add Lighthouse CI job with @lhci/cli

---
+++ lighthouserc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: "npm run start",
+      startServerReadyPattern: "Ready in",
+      url: [
+        "http://localhost:3000",
+        "http://localhost:3000/services",
+        "http://localhost:3000/book/tcreativestudio",
+      ],
+      numberOfRuns: 3,
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["error", { minScore: 0.9 }],
+        "categories:accessibility": ["error", { minScore: 0.95 }],
+        "categories:best-practices": ["error", { minScore: 0.95 }],
+        "categories:seo": ["error", { minScore: 0.95 }],
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
